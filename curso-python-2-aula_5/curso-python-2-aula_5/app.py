e59a857c688950ef745200b3712ddb1e56cdd47b
joaolucas01 <jlmota0412@gmail.com>
2025-10-26 10:51:34 -0300

    ;

---
+++ curso-python-2-aula_5/curso-python-2-aula_5/app.py
@@ -0,0 +1,13 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melancia', 5.0,'Grande')
+prato_paozinho = Prato('Paozinho',2.0,'O melhor pão da cidade')
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
